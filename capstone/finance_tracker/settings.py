ba7154cbfa0ee855012850a9d6e9fd121d3ea89b
satyavardhankoyalkar <145456047+satyavardhankoyalkar@users.noreply.github.com>
2025-03-30 11:56:09 +0530

    Update settings.py

--- capstone/finance_tracker/settings.py
+++ capstone/finance_tracker/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'finance_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 
 
 # Password validation
