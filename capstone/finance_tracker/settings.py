678f17e50b038717b1efb6160a5db7d1b214b0ae
satyavardhankoyalkar <145456047+satyavardhankoyalkar@users.noreply.github.com>
2025-03-30 13:00:36 +0530

    Update settings.py

--- capstone/finance_tracker/settings.py
+++ capstone/finance_tracker/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
      }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
